629880f31347d1c098de466b34e3130d0cf2aba8
Verónica González Pons <91873213+Veronicagnzpns@users.noreply.github.com>
2025-03-04 16:40:18 +0100

    userId

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -417,7 +417,7 @@ export interface ApiAdventurousAdventurous extends Schema.CollectionType {
     name: Attribute.String;
     surname: Attribute.String;
     email: Attribute.Email;
-    user: Attribute.Relation<
+    userId: Attribute.Relation<
       'api::adventurous.adventurous',
       'oneToOne',
       'plugin::users-permissions.user'
